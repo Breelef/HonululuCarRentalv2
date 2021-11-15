d669c2b565bf38e30598339f1b97ce0c0f78796a
xenius <xeni0103@stud.kea.dk>
2021-11-15 08:57:29 +0100

    Tilføjet switch i main (og lidt mere) tilføjet add metoder i Company og Private

--- src/com/company/Company.java
+++ src/com/company/Company.java
@@ -19,6 +19,7 @@ public class Company extends Customer {
     public Company() {
 
     }
+    //poelse
     //Add company customer method
     public static void addCompany(ArrayList<Company> companyList){      //IDE: skriv det i customer og så kun hav det ekstra her
         Scanner input = new Scanner(System.in);
