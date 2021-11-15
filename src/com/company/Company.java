68c005578dc312b6aeb39eb122a03a307854125d
breelef <emil_vinther@hotmail.com>
2021-11-15 09:09:23 +0100

    Filer og ændringer

--- src/com/company/Company.java
+++ src/com/company/Company.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Company extends Customer {
     String companyName;
@@ -7,16 +9,54 @@ public class Company extends Customer {
     int companyRegNr;
 
     public Company(String name, String address, int postNr, String city, int mobilePhone, int phone, String email,
-                   String companyName, String companyAddress, int companyNr, int companyRegNr) {
+                   String companyName, String companyAddress, int companyNr, int companyRegNr)  {
         super(name, address, postNr, city, mobilePhone, phone, email);
         this.companyName = companyName;
         this.companyAddress = companyAddress;
         this.companyNr = companyNr;
         this.companyRegNr = companyRegNr;
     }
-
     public Company() {
 
+    }
+    //Add company customer method
+    public static void addCompany(ArrayList<Company> companyList){      //IDE: skriv det i customer og så kun hav det ekstra her
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String nameT = input.nextLine();
+        System.out.println("Enter address: ");
+        String addressT = input.nextLine();
+        System.out.println("Enter postal code: ");
+        int postNrT = input.nextInt();
+        input.nextLine();       //Stops the next line from getting "eaten"
+        System.out.println("Enter city: ");
+        String cityT = input.nextLine();
+        System.out.println("Enter mobile number: ");
+        int mobilePhoneT = input.nextInt();
+        System.out.println("Enter phone number: ");
+        int phoneT = input.nextInt();
+        System.out.println("Enter email: ");
+        String emailT = input.next();
+        System.out.println("Enter company name: ");
+        String companyNameT = input.nextLine();
+        input.nextLine();       //Stops the next line from getting "eaten"
+        System.out.println("Enter company address: ");
+        String companyAddressT = input.nextLine();
+        System.out.println("Enter company phone number: ");
+        int companyNrT = input.nextInt();
+        System.out.println("Enter company registration number: ");
+        int companyRegNrT = input.nextInt();
+
+        Company companyAdd = new Company(nameT, addressT, postNrT, cityT, mobilePhoneT, phoneT, emailT,
+                companyNameT, companyAddressT, companyNrT, companyRegNrT);
+        companyList.add(companyAdd);
+
+    }
+    //Writes the companyArrayList to a file
+    public void writeCompanyToFile(ArrayList<Company> list)throws FileNotFoundException{
+        Scanner input = new Scanner(new File("Recourses/companyCustomer"));
+        while(input.hasNextLine()){
+        }
     }
 
     @Override
--- src/com/company/Private.java
+++ src/com/company/Private.java
@@ -1,22 +1,50 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Private extends Customer {
-    int licensePlate;
+    String licenseNr;
     String driverDate;
 
     public Private(String name, String address, int postNr, String city, int mobilePhone, int phone, String email,
-                   int licensePlate, String driverDate){
+                   String licenseNr, String driverDate){
         super(name, address, postNr,  city, mobilePhone, phone, email);
-        this.licensePlate = licensePlate;
+        this.licenseNr = licenseNr;
         this.driverDate = driverDate;
     }
     public Private(){
 
     }
+    public static void addPrivate(ArrayList<Private> privateList){  //IDE: skriv det i customer og så kun hav det ekstra her
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String nameT = input.nextLine();
+        System.out.println("Enter address: ");
+        String addressT = input.nextLine();
+        System.out.println("Enter postal code: ");
+        int postNrT = input.nextInt();
+        input.nextLine();       //Stops the next line from getting "eaten"
+        System.out.println("Enter city: ");
+        String cityT = input.nextLine();
+        System.out.println("Enter mobile number: ");
+        int mobilePhoneT = input.nextInt();
+        System.out.println("Enter phone number: ");
+        int phoneT = input.nextInt();
+        System.out.println("Enter email: ");
+        String emailT = input.next();
+        System.out.println("Enter the drivers license number: ");
+        String licenseNrT = input.next();
+        System.out.println("Enter the date that you acquired drivers license");
+        String driverDateT = input.next();
+
+        Private privateAdd = new Private(nameT, addressT, postNrT, cityT, mobilePhoneT, phoneT, emailT, licenseNrT, driverDateT);
+        privateList.add(privateAdd);
+    }
     @Override
     public String toString() {
         return ("Name: " +name+ "\nAddress: "+address+ "\nPostal code: "+postNr+ "\nCity: "+city+
                 "\nMobile phone number: "+mobilePhone +"\nPhone number: "+phone +"\nE-mail: "+email+
-                "\nLicense plate: "+licensePlate+ "\nDriver license acquired: "+driverDate);
+                "\nLicense plate: "+licenseNr+ "\nDriver license acquired: "+driverDate);
     }
 }
