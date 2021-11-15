552715f19595438d567ef93399b8ffa8a75671e9
BuFu9 <mads02p8@stud.kea.dk>
2021-11-15 11:35:06 +0100

    man kan nu tilføje luxury cars

--- src/com/company/Luxury.java
+++ src/com/company/Luxury.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Luxury extends Vehicle{
     int size;
     boolean gear;
@@ -16,7 +22,41 @@ public class Luxury extends Vehicle{
         seats = s;
     }
 
-    public Luxury(){
+    public static void addLuxury(ArrayList<Luxury> luxuryList)throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter brand name: ");
+        String b = input.nextLine();
+        System.out.println("Enter model name");
+        String m = input.nextLine();
+        System.out.println("Enter fuel for the car");
+        String f = input.nextLine();
+        System.out.println("Enter registration number");
+        int nr = input.nextInt();
+        System.out.println("Enter production year");
+        int year = input.nextInt();
+        System.out.println("Enter production month");
+        int month = input.nextInt();
+        System.out.println("Enter Total amount of travel distance of the car");
+        int om = input.nextInt();
+        System.out.println("Enter size of the car");
+        int str = input.nextInt();
+        System.out.println("Enter false for automatic gear (default for luxury cars!)");
+        boolean g = input.nextBoolean();
+        System.out.println("Enter false for AC (default for luxury cars!)");
+        boolean ac = input.nextBoolean();
+        System.out.println("Enter true for cruisecontrol (default for luxury cars!");
+        boolean cc = input.nextBoolean();
+        System.out.println("Enter seat material type");
+        String s = input.nextLine();
+
+
+        Luxury luxuryAdd = new Luxury(b, m, f, nr, year, month, om, str, g, ac, cc, s);
+        FileWriter fileWriter = new FileWriter("luxurycars.txt", true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(luxuryAdd + "\n\n");
+        bufferedWriter.close();
+        fileWriter.close();
+        luxuryList.add(luxuryAdd);
     }
 
     public String toString(){
@@ -24,7 +64,7 @@ public class Luxury extends Vehicle{
         if(gear){
             g = "Manuel gear";
         }else
-            g = "Automat gear";
+            g = "Automatic gear";
         return ("Mærke: "+brand+"\nModel: "+model+"\nBenzin type: "+fuel+"\nGear type: "+g+"\nAir condition: "+airCondition+
                 "\nCruise Control: "+cruiseControl+"\nType Sæde: "+seats+"\nStørrelse på bilen: "+size+"cm"+
                 "\nRegistreringsnummer: " +regNr+"\nRegistreringsdato: "+regMonth+"/"+regYear+"\nBilen har kørt: "+odoMeter+"km");
