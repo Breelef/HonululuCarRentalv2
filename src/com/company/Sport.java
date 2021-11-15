45a0e35ea90ef9822a1d231aaec5bbf94aefe47e
BuFu9 <mads02p8@stud.kea.dk>
2021-11-15 12:24:36 +0100

    man kan nu tilføje spots gog familie biler

--- src/com/company/Sport.java
+++ src/com/company/Sport.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Sport extends Vehicle{
     boolean gear;
     int HP;
@@ -10,7 +16,35 @@ public class Sport extends Vehicle{
         HP = hp;
     }
 
-    public Sport() {
+    public static void addSport(ArrayList<Sport> sportList)throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter brand name: ");
+        String b = input.nextLine();
+        System.out.println("Enter model name");
+        String m = input.nextLine();
+        System.out.println("Enter fuel for the car");
+        String f = input.nextLine();
+        System.out.println("Enter registration number");
+        int nr = input.nextInt();
+        System.out.println("Enter production year");
+        int year = input.nextInt();
+        System.out.println("Enter production month");
+        int month = input.nextInt();
+        System.out.println("Enter Total amount of travel distance of the car");
+        int om = input.nextInt();
+        System.out.println("Enter false for automatic gear (default for sports cars!)");
+        boolean g = input.nextBoolean();
+        System.out.println("Enter horsepower for the car" );
+        int hp = input.nextInt();
+
+
+        Sport sportAdd = new Sport(b, m, f, nr, year, month, om, g, hp);
+        FileWriter fileWriter = new FileWriter("Sports biler.txt", true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(sportAdd + "\n\n");
+        bufferedWriter.close();
+        fileWriter.close();
+        sportList.add(sportAdd);
     }
 
     @Override
